1eda557848f14204da6b5fb3468dcb83877f66b9
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-04-16 22:04:26 +0530

    Use of EJS

--- index.js
+++ index.js
@@ -1,59 +1,33 @@
+// EJS
+
 const express = require("express");
-const app = express(); // app is an object and express is function which returns some value.
+const app = express();
 
-//   console.dir(app);
+const path = require("path");
 
-// Creating Server Port
-let port = 8080;
+const port = 8080;
 
 app.listen(port, () => {
-  console.log("app is listening on port 8080");
+  console.log(`Listening on port ${port}`);
 });
 
-/***************************************************************************************************************************************** */
+app.set("views", path.join(__dirname, "/views")); // in case we start the server from outside view directory, this path code avoids errors.
 
-// Handling Request on Server and sending responce.
+// template engine
+app.set("view engine", "ejs");
 
-// app.use((req, res) => {
-//   console.log("Request Recived");
-//   // res.send("this is a basic responce");
-//   // res.send({
-//   //   name: "Shivraj",
-//   //   age: 23,
-//   //   edu: "masters in cs",
-//   let code =
-//     "<h1>fruits</h1><ul><li>Mango</li><li>Greps</li><li>apple</li></ul>";
-//   res.send(code);
-// }); 
-
-/***************************************************************************************************************************************** */
-
-//  Routing
-// get(), post() are the methods of sending requests.
-app.get("/search", (req, res) => {
-  res.send("You contacted the Search page.");
+app.get("/", (req, res) => {
+  res.render("home"); //home.ejs // .rendor is used to send ".ejs" file.
 });
 
-app.post("/home", (req, res) => {
-  res.send(`you contacted home page`);
+/**********************************************************************************/
+
+// Interpolation Syntax- EJS Tags :
+
+app.get("/rolldice", (req, res) => {
+
+  let diceVal = Math.floor(Math.random() *6)+1 ;
+  res.render("rollDice.ejs", { num: diceVal }); // diceVal value stored in num an that accept in rollDice.ejs file.
 });
 
-// if user send any false request that not present then by using * we can set default value for that all false requests.
 
-app.post("*", (req, res) => {
-  res.send(`this is wrong request...`)
-});
-
-/***************************************************************************************************************************************** */
-
-// path Parameters
-
-app.get("/:username/:id", (req, res) => {
-
-  // console.log(req.params);
-  // res.send("welcome to the your page")
-  //or
-
-  let {username, id} = req.params;
-  res.send(`welcome to the page ${username}`);
-});
