2c99bb25bf6cbd294cc43c6ca20d4c25c04f2070
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-04-27 21:44:24 +0530

    Update index.js

--- REST-API's-CRUD-Operations/index.js
+++ REST-API's-CRUD-Operations/index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+let port = 8080;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
+
+const path = require("path");
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
