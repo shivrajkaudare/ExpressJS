ab5d4e67cd29fedbef9b09010c6f64a877ad1935
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-05-30 18:20:47 +0530

    Update index.js

--- REST-API's-CRUD-Operations/index.js
+++ REST-API's-CRUD-Operations/index.js
@@ -12,7 +12,12 @@ app.listen(port, () => {
 
 // path require
 const path = require("path");
+// view folder
 app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
