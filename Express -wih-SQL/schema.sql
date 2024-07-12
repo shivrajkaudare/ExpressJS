78050a39b36045edcdfabbdf43ff857b80cf5cb5
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-12 20:56:01 +0530

    Add files via upload

---
+++ Express -wih-SQL/schema.sql
@@ -0,0 +1,8 @@
+/* we can connect node with js using sql file in sql file we can write sql commands */
+
+CREATE TABLE user(
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
