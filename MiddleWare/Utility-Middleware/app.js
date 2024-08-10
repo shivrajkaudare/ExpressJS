3647a40f4ab9a06a84a3969858d474932ca99c34
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-10 20:28:05 +0530

    logger middleware

---
+++ MiddleWare/Utility-Middleware/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// logger (middleware that log the information ex . morgen )
+app.use((req, res, next) => {
+  console.log(req.method, req.hostname, req.path);
+  next();
+});
+
+let port = "8080";
+
+app.get("/", (req, re) => {
+  res.send("hello i am root");
+});
+
+app.listen(port, () => {
+  console.log(`app is listing on port ${port}`);
+});
