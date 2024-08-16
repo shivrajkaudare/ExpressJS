defce840938d830c2d2e8b08f8e55804436dd1ea
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-16 19:13:52 +0530

    Update app.js

--- Error Handling/Custom Error Class/app.js
+++ Error Handling/Custom Error Class/app.js
@@ -12,3 +12,20 @@ const checkToken = (req, res, next) => {
   }
   throw new ExpressError(401, "ACCESS DENIED");
 };
+
+// route who returns the data
+app.get("/api", checkToken, (req, res) => {
+  res.send("data");
+});
+
+// default error message.
+app.use((err, req, res, next) => {
+  let { status, message } = err;
+  res.status(status).send(message);
+});
+
+let port = 8080;
+
+app.listen(port, () => {
+  console.log(`app is listing on port ${port}`);
+});
