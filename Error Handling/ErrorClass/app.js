88781c3e934cbab8f679345e174d47621df9a11e
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-17 15:45:05 +0530

    Express Error

--- Error Handling/ErrorClass/app.js
+++ Error Handling/ErrorClass/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const ExpressError = require("./ExpressError");
 
-// Creating a admin Route and it sending an error with a 403 status code.  
+// Creating a admin Route and it sending an error with a 403 status code.
+app.get("/admin", (req, res) => {
+  throw new ExpressError(403, "Access to Admin is Forbidden");
+});
 
 let port = 8080;
 
